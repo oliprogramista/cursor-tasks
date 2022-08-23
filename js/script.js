488d129f3403122996caef5461c47231873828eb
Olha Papezhuk <olka.pape@gmail.com>
2022-08-23 17:58:25 +0300

    log SUM_VALUE

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const MIN_VALUE = Math.min(priceApple, priceOrange, priceTomato);
 const SUM_VALUE = [priceApple + priceOrange + priceTomato];
 
 console.log(SUM_VALUE);
-document.writeln(SUM_VALUE);
+document.writeln("SUM_VALUE:" + SUM_VALUE);
 
 let priceOrangeFloor = Math.floor(priceOrange);
 let priceAppleFloor = Math.floor (priceApple);
